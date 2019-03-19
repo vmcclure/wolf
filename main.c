9a99f6b6fb080f41d1b8850129ea6c690e5b86d1
Viva Mcclure <vmcclure@e4r7p3.21-school.ru>
2019-03-19 22:45:24 +0300

    wolf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vmcclure <vmcclure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 19:40:52 by vmcclure          #+#    #+#             */
-/*   Updated: 2019/03/13 20:24:17 by vmcclure         ###   ########.fr       */
+/*   Updated: 2019/03/19 22:41:52 by vmcclure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,20 @@
 #include <math.h>
 #include "libft/libft.h"
 
-void render(int x, int y, int **map, int camx, int camy, SDL_Window *win, int x00, int y00, int pov, double newx, double newy)
+void render(int x, int y, int **map, double camx, double camy, SDL_Window *win, double x00, double y00, double pov, double newx, double newy)
 {
-	double lineheght[601];
+	long double lineheght[601];
 	int sx;
 	int sy;
-	float i;
+	double i;
 	int y1;
 	int x1;
 	double startx;
 	double starty;
 	double rayx;
 	double rayy;
+	SDL_Surface *surface;
+	
 	rayx = 0;
 	rayy = 0;
 	//int pov;
@@ -38,27 +40,38 @@ void render(int x, int y, int **map, int camx, int camy, SDL_Window *win, int x0
 	double dirx;
 	double diry;
 	double angle;
-	int stope;
-	double distance[601];
-	stope = 0;
-	//angle = pov - (fov/2);
-	newx += cos(angle* M_PI/180 );
-	newy += sin(angle* M_PI/180);
+	SDL_Texture *texture;
+	long double distance[601];
 	
+	
+	 
+	camx=camx+(cos((pov)* M_PI/180.0))*x00;
+	camy=camy+(sin((pov)* M_PI/180.0))*x00;
+	// angle = pov - (fov/2);
+	// newx = camx;//cos(angle* M_PI/180 );
+	// newy = camy;//sin(angle* M_PI/180);
+	// newx += cos(angle* M_PI/180 );
+	// newy += sin(angle* M_PI/180 );
 	// dirx = rayx + cos(angle* M_PI/180 );
 	// diry = rayy + sin(angle* M_PI/180);
 	
-	SDL_Texture *texture;
+	
     SDL_Renderer *renderer = NULL;
     SDL_Texture *bitmapTex = NULL;
     SDL_Surface *bitmapSurface = NULL;
     int posX = 1000, posY = 500, width = 600, height = 600;
 	SDL_Event event;
+	SDL_Texture *bmp1;
+	surface = SDL_LoadBMP("1.BMP");
 	renderer = SDL_CreateRenderer(win, 0, SDL_RENDERER_ACCELERATED);
-    
+	bmp1 = SDL_CreateTextureFromSurface(renderer, surface);
+    //texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
 		sx = width/(x);
 		sy = height/(y);
-		
+		int w;
+		int h;
+		//texture = SDL_CreateTextureFromSurface(renderer, surface);
+		//SDL_FreeSurface(surface);
         SDL_SetRenderDrawColor(renderer, 0, 255, 255, 0);
         SDL_RenderClear(renderer);
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
@@ -87,76 +100,121 @@ void render(int x, int y, int **map, int camx, int camy, SDL_Window *win, int x0
 			}   
 			y1++;
 		}
-		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-		
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+		// camx = camx+(cos(pov* M_PI/180 )*x00);
+		// camy = camy+(sin(pov* M_PI/180 )*x00);
 		i = -sx/2;
 		while (i < sy/2)
 		{
 			e = -sx/2;
 			while (e < sx/2)
 			{
-				SDL_RenderDrawPoint(renderer, camx*sx+i+x00, camy*sx+e+y00);
+				//SDL_RenderDrawPoint(renderer, camx*sx+i+x00, camy*sx+e+y00);
 				e++;
 			}
 			i++;
 		}
 		e = 0;
-		i = 0;
+		i = -30;
+		
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		while (i < 60)
+		// camx += (cos(pov* M_PI/180 ));
+		// camy += (sin(pov* M_PI/180 ));
+
+		while (i < 30)
 		{
 			rayx = 0;
 			rayy= 0;
+		//	 i += 30;
 			//pov = pov +i;
 			// if ((pov +i) - (fov/2) > 360)
 			// 	pov -= 360;
 			// if ((pov +i) - (fov/2)< 0)
 			// 	pov += 360;
-			angle = (pov +i) - (fov/2);
-			dirx = rayx + cos(angle* M_PI/180 );
-			diry = rayy + sin(angle* M_PI/180);
-			rayx = (camx*sx)+x00;
-			rayy = (camy*sx)+y00;
-			startx = rayx;
-			starty = rayy;
-			while ((int)rayx/sx < x && (int)rayy/sx < y && (map[((int)(rayy))/sy][((int)(rayx))/sx] != 1))
+			int k;
+			k = 0;
+			angle = (pov +i);// - (fov/2);
+			dirx = cos(angle* M_PI/180.0 );
+			diry = sin(angle* M_PI/180.0);
+			rayx = (camx);//+(cos(pov* M_PI/180))*x00));//+(cos(pov* M_PI/180))*x00;
+			rayy = (camy);//+(sin(pov* M_PI/180))*x00));//+(sin(pov* M_PI/180))*x00;
+			// camx = ((camx))+(cos(pov* M_PI/180))*x00;
+			// camy =((camy))+(sin(pov* M_PI/180))*x00;
+
+			startx = (rayx);
+			starty = (rayy);
+
+			while (rayx/sx < x && rayy/sx < y && map[(int)rayy/sy][(int)rayx/sx] != 1)
 			{
 				
-				SDL_RenderDrawPoint(renderer, rayx, rayy);
-				
-					rayx +=dirx;
-					rayy += diry;				
+
+				SDL_RenderDrawPoint(renderer, (rayx), (rayy));
+					rayx = (rayx + dirx) ;
+					rayy = (rayy + diry) ;				
 			}
+			//  rayx = (int)rayx;
+			//  rayy = (int)rayy;
 			
-			distance[e] = (pow(pow((rayx - startx) , 2)+ pow((rayy - starty),2),0.5));
-			distance[e] *= cos ((i-30)*M_PI/180) ;
-			lineheght[e] = 10000/distance[e];
+			distance[e] = sqrt(pow(rayx - startx , 2)+ pow(rayy- starty, 2));
+			//if (r % 10 == 0)
+			distance[e] = distance[e] * cos (i*M_PI/180.0);
+			lineheght[e] = 600 - distance[e] - (300 -distance[e] * cos (30 * M_PI/180));
+			printf("%Lf", lineheght[e]);
+			//lineheght[e] /= cos (i*M_PI/180.0);//*distance[e]/distance[e];
+			//(long double)1000/
+			printf ("%f\n", roundf(distance[e]));
 			e++;
-			i+=0.1;
-			//printf("%f\n", lineheght[0]);
+			i = i + 0.1;
+
 		}
 		x1 = 0;
 		y1 = 0;
 		i = 0;
 		e = 0;
-		printf ("%f\n",cos (angle*M_PI/180));
-		printf("%f\n", lineheght[0]);
-		printf("%f\n",distance[(int)i]);
-		//SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+	int k;
+	k = 0;
+	float z;
+	z =0;
+	float raz;
+	float p;
+	p =0;
+	float l;
+		SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
 		while (e < 600)
 		{	
+			// if (e%sx == 0  && e < 600)
+			// 	{
+			// 		z = lineheght[(e+sx-1)]-lineheght[e];
+			// 		l = lineheght[e];
+			// 		k = 0;
+					
+			// 		raz = z/sx;
+			// 		printf("%f\n",raz);
+			// 		while (k < sx)
+			// 		{
+			// 			lineheght[k+e] = l+(raz*k);
+			// 			k++;
+			// 		}
+			// 	}
 			y1 = 0;
-			while (y1 < lineheght[e])
+			while (y1 < (lineheght[e]))
 			{
-				SDL_SetRenderDrawColor(renderer,lineheght[e]/2 , 0, lineheght[e]/4, 255);
-				SDL_RenderDrawPoint(renderer, e, 600-y1);
+				
+				SDL_SetRenderDrawColor(renderer,lineheght[e]/2 , 0, lineheght[e]/2, 255);
+				SDL_RenderDrawPoint(renderer, e, 300 - (lineheght[e] / 2) + y1);
 				y1++;
 			}
 			e++;
 		}
-		
+		SDL_Rect pos;
+		pos.x = 0;
+		pos.y = 0;
+		//SDL_QueryTexture(bmp1, NULL, NULL, &pos.w, &pos.h);
+		pos.w = 20;
+		pos.h = 30;
+		SDL_RenderCopy(renderer, bmp1, NULL, &pos);
         SDL_RenderPresent(renderer);
-
+	//printf("%d",sx);
 		while (1) {
 			
      	   if (SDL_PollEvent(&event))
@@ -171,12 +229,12 @@ void render(int x, int y, int **map, int camx, int camy, SDL_Window *win, int x0
 			SDL_DestroyRenderer(renderer);
    				if(event.key.keysym.sym == SDLK_RIGHT)
 				{
-					x00 += 4;
+					x00 =8;
 					render(x, y, map, camx, camy, win, x00, y00, pov, newx, newy);
 				}
 				if(event.key.keysym.sym == SDLK_LEFT)
 				{
-					x00 -= 4;
+					x00 = -8;
 					render(x, y, map, camx, camy, win, x00, y00, pov, newx, newy);
 				}
     			if(event.key.keysym.sym == SDLK_UP)
@@ -191,16 +249,18 @@ void render(int x, int y, int **map, int camx, int camy, SDL_Window *win, int x0
 				}
 				if(event.key.keysym.sym == SDLK_d)
 				{
+					x00 = 0;
 					pov += 1;
-					// if (pov > 360)
-					// 	pov -= 360;
-					// if (pov < 0)
-					// 	pov += 360;
+					if (pov > 360)
+						pov -= 360;
+					if (pov < 0)
+						pov += 360;
 					render(x, y, map, camx, camy, win, x00, y00, pov, newx, newy);
 				}
 				if(event.key.keysym.sym == SDLK_a)
 				{
 					pov -= 1;
+					x00=0;
 					render(x, y, map, camx, camy, win, x00, y00, pov, newx, newy);
 				}
 }
@@ -311,9 +371,10 @@ int main(int c, char *v[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
      win = SDL_CreateWindow("Hello World", posX, posY, width, height, 0);
-	 camx = 5;
-		camy = 5;
-	render(x, y, map, camx, camy, win, 0 , 0, 0, 0, 0);
+	camx = 12;
+	camy = 5;
+	sx = 600/x;
+	render(x, y, map, camx*sx, camy*sx , win, 0 , 0, 0, 0, 0);
     SDL_DestroyWindow(win);
 
     SDL_Quit();
---
+++ test.c
@@ -0,0 +1,92 @@
+#include "include/SDL2/SDL.h"
+#include "wolf.h"
+/* Moving Rectangle */
+void line_DDA(HDC hdc, float x1, float y1, float x2, float y2)
+{
+	// Целочисленные значения координат начала и конца отрезка,
+	// округленные до ближайшего целого
+	int iX1 = roundf(x1);
+	int iY1 = roundf(y1);
+	int iX2 = roundf(x2);
+	int iY2 = roundf(y2);
+ 
+	// Длина и высота линии
+	int deltaX = abs(iX1 - iX2);
+	int deltaY = abs(iY1 - iY2);
+ 
+	// Считаем минимальное количество итераций, необходимое
+	// для отрисовки отрезка. Выбирая максимум из длины и высоты
+	// линии, обеспечиваем связность линии
+	int length = max(deltaX, deltaY);
+ 
+	// особый случай, на экране закрашивается ровно один пиксел
+	if (length == 0)
+	{
+		SetPixel (hdc, iX1, iY1, 0);
+		return;
+	}
+ 
+	// Вычисляем приращения на каждом шаге по осям абсцисс и ординат
+	double dX = (x2 - x1) / length;
+	double dY = (y2 - y1) / length;
+ 
+	// Начальные значения
+	double x = x1;
+	double y = y1;
+ 
+	// Основной цикл
+	length++;
+	while (length--)
+	{
+		x += dX;
+		y += dY;
+		SetPixel(hdc, roundf(x), roundf(y), 0);
+	}
+}
+int main(int argc, char *argv[])
+{
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        SDL_Texture *texture;
+        SDL_Event event;
+        SDL_Rect r;
+
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+                return 3;
+        }
+
+        window = SDL_CreateWindow("SDL_CreateTexture",
+                        SDL_WINDOWPOS_UNDEFINED,
+                        SDL_WINDOWPOS_UNDEFINED,
+                        1024, 768,
+                        SDL_WINDOW_RESIZABLE);
+
+        r.w = 100;
+        r.h = 50;
+
+        renderer = SDL_CreateRenderer(window, -1, 0);
+
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
+
+        while (1) {
+                SDL_PollEvent(&event);
+                if(event.type == SDL_QUIT)
+                        break;
+                r.x=rand()%500;
+                r.y=rand()%500;
+
+                SDL_SetRenderTarget(renderer, texture);
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+                SDL_RenderClear(renderer);
+                SDL_RenderDrawRect(renderer,&r);
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
+                SDL_RenderFillRect(renderer, &r);
+                SDL_SetRenderTarget(renderer, NULL);
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+        }
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return 0;
+}
